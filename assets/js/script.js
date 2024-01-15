d47d2ea71d041168b853065f3045580074e575a0
Rahul Bhati <rb083380@gmail.com>
2024-01-15 17:33:17 +0530

    add toggle functionality for sidebar in  small screen size

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,28 @@
+// const navToggle = document.querySelector('.nav-toggle');
+// const sidebar = document.querySelector('.sidebar');
+
+// console.log(sidebar, navToggle);
+// navToggle.addEventListener('click', () => {
+//      sidebar.classList.toggle('sidebar--visible');
+// })
+
 const navToggle = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.nav');
+const sidebar = document.querySelector('.sidebar');
 
+console.log(sidebar, navToggle);
 
+const flag = false;
 navToggle.addEventListener('click', () => {
-     nav.classList.toggle('nav--visible');
+     // if(!flag) {
+     //   sidebar.style =  `display: flex;
+     // position: absolute;
+     // width: min(100%,545px);
+     // z-index: 2;
+     // visibility:visible;`;
+
+     // console.log(sidebar.style);
+     // } else {
+     //   sidebar.style =  `display: none`;
+     // }
+     sidebar.classList.toggle('sidebar--visible');
 })
